1b1cb26f95ab92afcf57caae3392d8d2c24b8c02
CoolManGame <nghiagmt@gmail.com>
2023-07-08 01:57:05 +0700

    Xóa node_modules
    Folder xàm loz này cay vl

--- node_modules/.bin/browsers.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\@puppeteer\browsers\lib\cjs\main-cli.js" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\@puppeteer\browsers\lib\cjs\main-cli.js" %*
-)
